289f549d6e97107368e07b12a50ccfe8cc69d07b
nazi sharifi <banonazisharifi@gmail.com>
2025-09-26 22:43:37 +0430

    cOMPONNINTS ADDED

--- src/MyComponent/Card.tsx
+++ src/MyComponent/Card.tsx
@@ -27,7 +27,7 @@ function Card() {
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 4,
+          slidesToShow: 3,
           slidesToScroll: 1,
           infinite: true,
           initialSlide: 1,
@@ -68,17 +68,18 @@ function Card() {
             bg:"/img/hero4.webp",
             logo:"/img/Group_25741_9ef66ca4-0f60-4e5a-9b2a-64802ff82a61.webp"
         },
+       
     ]
   return (
-    <div className='w-full lg:h-34  flex'>
+    <div className='w-full  flex'>
         <Slider {...settings} className='w-full'>
         {imges.map((obj,index)=>(
-        <div key={index} className='lg:h-34 h-34 lg:w-55 md:w-55 w-44 bg-cover bg-no-repeat  relative bg-center group' style={{ backgroundImage: `url(${obj.bg})` }}>
+        <div key={index} className='lg:h-34 h-34 lg:w-1/4 md:w-1/3 w-full bg-cover bg-no-repeat  relative bg-center group' style={{ backgroundImage: `url(${obj.bg})` }}>
             <img src={obj.bg} className='h-34 -z-50 w-55 absolute top-0 right-1' alt="" />
         <div className='h-full w-56 z-50 bg-stone-800 flex flex-col justify-center items-center p-1 ml-0 group-hover:w-0 transition-all duration-700 ease-linear  text-white'>
         
         </div>
-        <img src={obj.logo} className=' absolute md:top-12 top-12 lg:top-6 ' alt="" />
+        <img src={obj.logo} className=' absolute lg:left-5 md:top-12 top-12 lg:top-6 ' alt="" />
         </div>
         ))}
         </Slider>
