590eb0ff31a6961dfb8646d670451fcfd192b95a
nazi sharifi <banonazisharifi@gmail.com>
2025-10-15 21:09:47 +0430

    Componint added

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import './index.css' // فایل Tailwind CSS
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
-})
+  plugins: [
+    tailwindcss(),
+    react()],
+  optimizeDeps: {
+    include: ["react-slick", "slick-carousel/slick/slick.css", "slick-carousel/slick/slick-theme.css"],
+  },
+});
+
