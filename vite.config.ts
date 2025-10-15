d72da559f8be4e179e16f0cd53bf2e1ea9c3b87d
nazi sharifi <banonazisharifi@gmail.com>
2025-10-15 21:46:12 +0430

    Componint added

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,14 @@ export default defineConfig({
     tailwindcss(),
     react()],
   optimizeDeps: {
-    include: ["react-slick", "slick-carousel/slick/slick.css", "slick-carousel/slick/slick-theme.css"],
+    include: [
+      "react-slick",
+      "slick-carousel/slick/slick.css",
+      "slick-carousel/slick/slick-theme.css"
+    ],
+  },
+  css: {
+    devSourcemap: true,
   },
 });
 
