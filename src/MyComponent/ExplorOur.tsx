60ccbbe79aa73e1b3b2346a96600407118d48bce
nazi sharifi <banonazisharifi@gmail.com>
2025-10-15 21:04:21 +0430

    Componint added

--- src/MyComponent/ExplorOur.tsx
+++ src/MyComponent/ExplorOur.tsx
@@ -78,30 +78,38 @@ function ExplorOur() {
         },
     ]
   return (
-    <div className='h-fit p-12 w-full flex justify-center flex-col items-center gap-5'>
-        <h3 className='text-amber-600'>EXPLORE OUR</h3>
-        <h1 className='lg:text-5xl md:text-4xl text-2xl text-white'>Luxurious Haven Collection</h1>
-        <div className='flex flex-wrap w-full justify-between items-center space-x-2'>
-            <Slider {...settings} className='w-full flex justify-between gap-5'>
-            {data.map((item,index)=>(
-            <div key={index} className='h-[500px] md:w-[300px] transition-all duration-1000 ease-linear rounded-2xl flex flex-col justify-center group relative items-center gap-3 '>
-                <img src={item.img}  className='w-full h-[350px] rounded-2xl' alt="" />
-                <p className='text-gray-600 text-center'>{item.p}</p>
-                <h1 className='text-2xl text-white text-center'>{item.h1}</h1>
-                <h3 className='text-white text-center'>{item.h3}</h3>
-                <div className={`h-[350px] w-full rounded-2xl flex-col hidden group-hover:block items-end bg-cover absolute top-0 transition-all duration-1000 ease-linear right-0 bg-no-repeat `} style={{backgroundImage:`url(${item.bg})`}}>
-                <div className='flex flex-col items-end gap-2 p-4  text-white text-xl'>
-                <i className='p-3 rounded-full bg-gray-500/50 fas fa-magnifying-glass'></i>
-                <i className='p-3 rounded-full bg-gray-500/50 fas fa-heart'></i>
-                <i className='p-3 rounded-full bg-gray-500/50 fas fa-arrows-rotate'></i>
-                <i className='p-3 rounded-full bg-gray-500/50 fas fa-cart-shopping'></i>
-                </div>
-                </div>
+   <div className='h-fit p-12 w-full flex flex-col justify-center items-center gap-5'>
+  <h3 className='text-amber-600'>EXPLORE OUR</h3>
+  <h1 className='lg:text-5xl md:text-4xl text-2xl text-white'>Luxurious Haven Collection</h1>
+
+  <div className='w-full'>
+    <Slider {...settings} className='w-full'>
+      {data.map((item, index) => (
+        <div
+          key={index}
+          className='h-[500px] w-full transition-all duration-700 ease-linear rounded-2xl flex flex-col justify-center group relative items-center gap-3'
+        >
+          <img src={item.img} className='w-full h-[350px] rounded-2xl' alt='' />
+          <p className='text-gray-600 text-center'>{item.p}</p>
+          <h1 className='text-2xl text-white text-center'>{item.h1}</h1>
+          <h3 className='text-white text-center'>{item.h3}</h3>
+          <div
+            className='h-[350px] w-full rounded-2xl flex-col hidden group-hover:flex items-end bg-cover absolute top-0 transition-all duration-700 ease-linear right-0 bg-no-repeat'
+            style={{ backgroundImage: `url(${item.bg})` }}
+          >
+            <div className='flex flex-col items-end gap-2 p-4 text-white text-xl'>
+              <i className='p-3 rounded-full bg-gray-500/50 fas fa-magnifying-glass'></i>
+              <i className='p-3 rounded-full bg-gray-500/50 fas fa-heart'></i>
+              <i className='p-3 rounded-full bg-gray-500/50 fas fa-arrows-rotate'></i>
+              <i className='p-3 rounded-full bg-gray-500/50 fas fa-cart-shopping'></i>
             </div>
-            ))}
-            </Slider>
+          </div>
         </div>
-    </div>
+      ))}
+    </Slider>
+  </div>
+</div>
+
   )
 }
 
