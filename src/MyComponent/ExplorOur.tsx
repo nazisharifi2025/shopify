53bdb3dfe75d421329246797cd92231c66f02b20
nazi sharifi <banonazisharifi@gmail.com>
2025-10-15 22:03:24 +0430

    Componint added

--- src/MyComponent/ExplorOur.tsx
+++ src/MyComponent/ExplorOur.tsx
@@ -2,42 +2,45 @@
 import Slider from 'react-slick';
 import type { Settings } from "react-slick";
 function ExplorOur() {
-      const settings: Settings = {
-  dots: true,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 1500,
-  arrows: true,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        dots: true,
-      },
+ const settings: Settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    initialSlide: 1,
+    autoplay: true,
+    arrows: false,
+    autoplaySpeed: 1500,
+ responsive: [
+  {
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      infinite: true,
+      dots: false,
     },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        dots: false,
-      },
+  },
+  {
+    breakpoint: 600,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      initialSlide: 1,
     },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: false,
-      },
+  },
+  {
+    breakpoint: 480,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
     },
-  ],
-};
+  },
+],
+  };
     const data = [
         {
             img: "/img/shop-4-01.webp",
