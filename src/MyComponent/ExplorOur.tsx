ec7bc7cf0fc516a1a7dc45ddb0a8268a556a2582
nazi sharifi <banonazisharifi@gmail.com>
2025-10-15 10:22:51 +0430

    Componint added

--- src/MyComponent/ExplorOur.tsx
+++ src/MyComponent/ExplorOur.tsx
@@ -18,7 +18,7 @@ function ExplorOur() {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 4,
+          slidesToShow: 3,
           slidesToScroll: 1,
           infinite: true,
           dots: false,
