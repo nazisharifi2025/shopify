2c3bcdc8361dc3f26a4dd5f7ad8e17dc61459a4b
nazi sharifi <banonazisharifi@gmail.com>
2025-10-15 21:20:56 +0430

    Componint added

--- src/MyComponent/Blogpart.tsx
+++ src/MyComponent/Blogpart.tsx
@@ -14,32 +14,32 @@ export default function Blogpart (){
                  <p className="text-white lg:text-base text-2xl lg:pt-0 pt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam iusto amet fuga mollitia repudiandae! Repellat </p>
                </div>
             </div>
-            <div className="lg:w-1/2 w-full lg:h-[80%] md:h-[80%] h-fit flex flex-col lg:gap-5 md:gap-9 lg:pl-0 pl-3 gap-7 ">
-            <div className="flex gap-6 items-center">
+            <div className="lg:w-1/2 w-full lg:h-[80%] md:h-[80%] h-fit flex flex-col lg:gap-5 md:gap-9  lg:pl-0 pl-3 gap-7 ">
+            <div className="flex gap-6 md:flex-row flex-col items-center">
                 <div>
-                    <img className="hover:scale-110 h-48 w-56 rounded-2xl" src="/img/blog-05.jpg" alt="" />
+                    <img className="hover:scale-110 h-48 md:w-56 w-full rounded-2xl" src="/img/blog-05.jpg" alt="" />
                 </div>
-                <div className="flex flex-col  w-[50%] h-48 justify-between">
+                <div className="flex flex-col  md:w-[50%] w-full h-48 justify-between">
                     <p className="text-sm text-amber-700">MAY 27,2024</p>
                     <h1 className="lg:text-2xl md:text-2xl text-lg  font-light text-white ">Tips For designing Reading Area Space Smartly</h1>
                     <p className="font-light text-neutral-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam rerum...</p>
                 </div>
             </div>
-             <div className="flex gap-6 items-center">
+             <div className="flex gap-6 md:flex-row flex-col items-center">
                 <div>
-                    <img className="hover:scale-110 h-40 w-56 rounded-2xl" src="/img/blog.webp" alt="" />
+                    <img className="hover:scale-110 h-40 md:w-56 w-full rounded-2xl" src="/img/blog.webp" alt="" />
                 </div>
-                <div className="flex flex-col  w-[50%] h-40 justify-between">
+                <div className="flex flex-col  md:w-[50%] w-full h-40 justify-between">
                     <p className="text-sm text-amber-700">MAY 27,2024</p>
                     <h1 className="lg:text-2xl md:text-2xl text-lg font-light text-white ">Easy Way To Furnish Your Home Affordably</h1>
                     <p className="font-light text-neutral-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam rerum...</p>
                 </div>
             </div>
-             <div className="flex gap-6 items-center">
+             <div className="flex gap-6 md:flex-row flex-col items-center">
                 <div>
-                    <img className="hover:scale-110 h-40 w-56 rounded-2xl" src="/img/newsletter.webp" alt="" />
+                    <img className="hover:scale-110 h-40 md:w-56 w-full rounded-2xl" src="/img/newsletter.webp" alt="" />
                 </div>
-                <div className="flex flex-col  w-[50%] h-40 justify-between">
+                <div className="flex flex-col  md:w-[50%] w-full h-40 justify-between">
                     <p className="text-sm text-amber-700">MAY 27,2024</p>
                     <h1 className="lg:text-2xl md:text-2xl text-lg  font-light text-white ">Minimalist Modern Modular Kitchen Design</h1>
                     <p className="font-light text-neutral-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam rerum...</p>
