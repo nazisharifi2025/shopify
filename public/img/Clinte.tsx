3f7b47fa4fb89d98673cfb75684a874528883123
nazi sharifi <banonazisharifi@gmail.com>
2025-10-28 21:12:17 +0430

    some  style added

---
+++ public/img/Clinte.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Clinte() {
+  return (
+    <div className="text-black bg-white flex justify-center items-center gap-4 font-bold text-6xl font-serif">
+        <h1>Hi There</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae dolores aperiam fuga, et minima nemo iste quibusdam cupiditate, nostrum assumenda eius dolore, vitae voluptatum libero officiis illo temporibus rem id?</p>
+    </div>
+  )
+}
+
+export default Clinte
